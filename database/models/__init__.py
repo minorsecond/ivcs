6b528ea62f7cd4a3e39e43a69ea8d148ce3c53bf
Ross Wardrup <minorsecond@gmail.com>
2016-09-08 17:54:09 -0500

    IVCS-2 Write DB backend
    Began declaration of projects table

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -6,7 +6,14 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, create_engine
 from sqlalchemy.types import DateTime
 from sqlalchemy.orm import relationship
+from database.base import Base
 
 
-class Imagery(Base):
-    
+class Projects(Base):
+    """
+    Table for storing all projects (will be stored on users local machine)
+    """
+
+    __tablename__ = "projects"
+    id = Column(Integer, primary_key=True, index=True)
+
