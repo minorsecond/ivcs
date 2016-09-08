a901dc2f27f1b118499bf4dd2b6bce392b13087d
Ross Wardrup <minorsecond@gmail.com>
2016-09-08 17:22:14 -0500

    IVCS-4 Create main window
    Added create branch.ui and added additional lines to setup.sh

--- ivcs.py
+++ ivcs.py
@@ -4,6 +4,33 @@ Imagery Version Control System
 """
 
 import os
+import sys
 import shutil
 import hashlib
 import datetime
+from gui import commit_message_window, ivcs_mainwindow, settings_window, view_message_window
+
+
+class MainWindow(ivcs_mainwindow.QtGui.QMainWindow, ivcs_mainwindow.Ui_MainWindow):
+    def __init__(self):
+        ivcs_mainwindow.QtGui.QMainWindow.__init__(self)
+        ivcs_mainwindow.Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        #self.setFixedHeight(self.size())
+
+        # Disable all GUI elements until a branch is set and scanned
+        self.RemoteFileList.setEnabled(False)
+        self.LocalFileList.setEnabled(False)
+        self.RemoteChangeList.setEnabled(False)
+        self.LocalChangeList.setEnabled(False)
+
+        self.CheckoutButton.setEnabled(False)
+        self.ViewRemoteCommitButton.setEnabled(False)
+        self.CommitButton.setEnabled(False)
+        self.PushButton.setEnabled(False)
+
+if __name__ == '__main__':
+    app = ivcs_mainwindow.QtGui.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
--- setup.sh
+++ setup.sh
@@ -8,4 +8,6 @@ pyuic4 gui/ivcs_mainwindow.ui -o gui/ivcs_mainwindow.py
 pyuic4 gui/settings_window.ui -o gui/settings_window.py
 pyuic4 gui/view_message_window.ui -o gui/view_message_window.py
 pyuic4 gui/commitmessage_window.ui -o gui/commit_message_window.py
+pyuic4 gui/CreateBranchWindow.ui -o gui/CreateBranchWindow.py
+pyuic4 gui/CheckoutStatus.ui -o gui/CheckoutStatus.py
 
