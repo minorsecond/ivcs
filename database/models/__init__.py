dd88876a081cda9c93fc6fc79217937c8d0f1586
Ross Wardrup <minorsecond@gmail.com>
2016-09-08 17:51:43 -0500

    IVCS-2 Write DB backend
    Added instantiation of declarive base in another file

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -6,3 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, create_engine
 from sqlalchemy.types import DateTime
 from sqlalchemy.orm import relationship
+
+
+class Imagery(Base):
+    
