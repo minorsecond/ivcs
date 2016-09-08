17453545c32c47877672e39fae994c3ec1a9b453
Ross Wardrup <minorsecond@gmail.com>
2016-09-08 17:26:41 -0500

    IVCS-4 Create main window
    Added db directory

---
+++ database/models/__init__.py
@@ -0,0 +1,8 @@
+"""
+Contains the SQL database definitions
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, create_engine
+from sqlalchemy.types import DateTime
+from sqlalchemy.orm import relationship
