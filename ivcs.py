0a23f2797d1f910df215f9982b54f098c58a6752
Ross Wardrup <minorsecond@gmail.com>
2016-09-08 17:48:58 -0500

    IVCS-2 Write DB backend
    Added ini file creation

--- ivcs.py
+++ ivcs.py
@@ -4,6 +4,7 @@ Imagery Version Control System
 """
 
 import os
+import configparser
 import sys
 import shutil
 import hashlib
@@ -16,21 +17,79 @@ class MainWindow(ivcs_mainwindow.QtGui.QMainWindow, ivcs_mainwindow.Ui_MainWindo
         ivcs_mainwindow.QtGui.QMainWindow.__init__(self)
         ivcs_mainwindow.Ui_MainWindow.__init__(self)
         self.setupUi(self)
-        #self.setFixedHeight(self.size())
+        # elf.setFixedHeight(self.size())
 
-        # Disable all GUI elements until a branch is set and scanned
-        self.RemoteFileList.setEnabled(False)
-        self.LocalFileList.setEnabled(False)
-        self.RemoteChangeList.setEnabled(False)
-        self.LocalChangeList.setEnabled(False)
+        # Create config file
+        app_dir = get_application_path()
+        if not os.path.exists(os.path.join(app_dir, 'ivcs.ini')):
 
-        self.CheckoutButton.setEnabled(False)
-        self.ViewRemoteCommitButton.setEnabled(False)
-        self.CommitButton.setEnabled(False)
-        self.PushButton.setEnabled(False)
+            # Disable all GUI elements until a branch is set and scanned
+            self.RemoteFileList.setEnabled(False)
+            self.LocalFileList.setEnabled(False)
+            self.RemoteChangeList.setEnabled(False)
+            self.LocalChangeList.setEnabled(False)
 
-if __name__ == '__main__':
+            self.CheckoutButton.setEnabled(False)
+            self.ViewRemoteCommitButton.setEnabled(False)
+            self.CommitButton.setEnabled(False)
+            self.PushButton.setEnabled(False)
+
+def get_application_path():
+    """
+    Gets the location of the executable file for storing data
+    :return: None
+    """
+
+    application_path = None
+
+    if getattr(sys, 'frozen', False):
+        application_path = os.path.dirname(sys.executable)
+
+    elif __file__:
+        application_path = os.path.dirname(__file__)
+
+    return application_path
+
+
+def initialize_config(path):
+    """
+    Sets up a new config file, or loads one if it already exists at the exe directory
+    :param path: path to config file
+    :return: None
+    """
+
+    config_file_path = os.path.join(path, 'ivcs.ini')
+
+    config = configparser.ConfigParser()
+
+    # Set default options
+    config['DEFAULT'] = {"ImageExtensions": ['.img', '.tif'],
+                         "ChangeDetectMethod": "Timestamp",
+                         "Username": "UNSET"}
+
+    config['BRANCHES'] = {"TestBranch": "~/DEV/ivcs/testBranch"}
+
+    with open(config_file_path, 'w') as configfile:
+        config.write(configfile)
+
+
+def main():
+    """
+    Starts the program
+    :return: None
+    """
+
+    # Create config file
+    app_dir = get_application_path()
+
+    if not os.path.exists(os.path.join(app_dir, 'ivcs.ini')):
+        initialize_config(app_dir)
+
     app = ivcs_mainwindow.QtGui.QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
